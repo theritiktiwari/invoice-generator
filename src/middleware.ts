2d3b0600ec00fd1b3e4b0be16c182e9aefe62714
Ritik Tiwari <dev.ritiktiwari@gmail.com>
2024-04-20 14:15:58 +0530

    Middleware added

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from 'next-auth/jwt'
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+    const token = await getToken({ req: request })
+    const url = request.nextUrl;
+
+    if (token && (
+        url.pathname === '/sign-in' ||
+        url.pathname === '/sign-up' ||
+        url.pathname === '/verify'
+    )) {
+        return NextResponse.redirect(new URL('/dashboard', request.url))
+    }
+    return NextResponse.redirect(new URL('/sign-in', request.url))
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: [
+        '/sign-in',
+        '/sign-up',
+        '/dashboard/:path*',
+        '/verify/:path*',
+        '/generate',
+        '/invoice/:path*',
+    ]
+}
