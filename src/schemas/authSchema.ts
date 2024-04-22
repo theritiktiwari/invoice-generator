e5b30f9b232256110b2ebf3151a519553dd7323d
Ritik Tiwari <dev.ritiktiwari@gmail.com>
2024-04-22 19:57:01 +0530

    Schema Updated

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -1,8 +1,18 @@
 import { z } from "zod";
 
+export const signInSchema = z.object({
+    email: z.string().email({ message: "Invalid email address" }),
+    password: z.string().min(6, { message: "Password must be at least 6 characters" }),
+});
+
 export const signUpSchema = z.object({
     firstName: z.string().min(2, { message: "First name must be at least 2 characters" }),
     lastName: z.string().min(2, { message: "Last name must be at least 2 characters" }).optional().or(z.literal('')),
     email: z.string().email({ message: "Invalid email address" }),
     password: z.string().min(6, { message: "Password must be at least 6 characters" }),
+});
+
+export const verifySchema = z.object({
+    email: z.string().email({ message: "Invalid email address" }),
+    code: z.string().length(6, { message: "Verification code must be at least 6 digits" })
 });
--- src/schemas/verifySchema.ts
+++
@@ -1,5 +0,0 @@
-import { z } from "zod";
-
-export const verifySchema = z.object({
-    code: z.string().length(6, { message: "Verification code must be at least 6 digits" })
-});
