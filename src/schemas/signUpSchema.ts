61442d77c675aecdbc0552540f984f44ae80f685
Ritik Tiwari <dev.ritiktiwari@gmail.com>
2024-04-22 12:30:28 +0530

    Schema updated

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
 export const signUpSchema = z.object({
-    email: z.string().email({ message: "Invalid email" }),
+    firstName: z.string().min(2, { message: "First name must be at least 2 characters" }),
+    lastName: z.string().min(2, { message: "Last name must be at least 2 characters" }).optional().or(z.literal('')),
+    email: z.string().email({ message: "Invalid email address" }),
     password: z.string().min(6, { message: "Password must be at least 6 characters" }),
 });
